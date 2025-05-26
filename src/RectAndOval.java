a5b808e6700876a9dda810729d84d6fb2aba3cf7
eaharuto <kamitom0115@gamail.com>
2025-05-26 11:33:35 +0900

    TextTest

---
+++ src/RectAndOval.java
@@ -0,0 +1,10 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		fillRect(170,70,100,90);
+		fillRect(120,110,200,50);
+		fillOval(130, 160, 40, 40);
+		fillOval(270, 160, 40, 40);
+	}
+
+}
