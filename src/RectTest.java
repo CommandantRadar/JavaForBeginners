365a0078f3251e6a29b35b09501f0530577e6817
eaharuto <kamitom0115@gamail.com>
2025-05-26 10:21:46 +0900

    setColor

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,6 +1,22 @@
 
 public class RectTest extends MyFrame {
 	public void run() {
-		fillRect(30,50,200,100);
+		setColor(255,255,0);
+		fillRect(30,40,50,100);
+		
+		setColor(0,222,0);
+		fillRect(50,100,40,30);
+		
+		setColor(0,255,0);
+		fillRect(130,40,50,100);
+		
+		setColor(175,0,0);
+		fillRect(50,30,10,40);
+		
+		setColor(0,0,250);
+		fillRect(30,200,150,10);
+		
+		setColor(255,255,255);
+		fillRect(40,90,30,10);
 	}
 }
