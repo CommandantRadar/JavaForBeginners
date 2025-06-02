06aed36819b9d1efee312b77cad7e2ac5dbc056c
eaharuto <kamitom0115@gamail.com>
2025-06-02 09:18:29 +0900

    Rectifの追加

---
+++ src/Rectif.java
@@ -0,0 +1,219 @@
+
+public class Rectif extends MyFrame {
+	public void run() {
+	 //4-1
+		//int x = 30;
+		//setColor(0, 128, 0);
+		//for(int i = 0; i < 10; i++) {
+		//fillRect(x,x,10,100);
+
+		//x+=20;
+		//}
+		//for (int i = 0; i < 10; i++) {
+			//fillRect(x, 100, 10, x);
+		//}
+	//}
+//}
+    //4-2
+		//int x = 30;
+		//setColor(0,128,0);
+		//int i;
+		//for(i = 0; i < 10; i++) {
+			//if(i==0) {
+				//fillRect(x,80,10,10);
+				
+			  //}
+			    //else if(i==1) {
+			    //fillRect(x,80,10,30);
+			  //}
+			    //else if(i==2) {
+			    //fillRect(x,80,10,60);
+			    
+			  //}
+			    //else if(i==3) {
+			    //fillRect(x,80,10,80);
+			  
+			  //}
+			    //else if(i==4) {
+			    //fillRect(x,80,10,100);
+			    
+			  //}
+			    //else if(i==5) {
+			    //fillRect(x,80,10,120);
+			    
+			  //}
+			    //else if(i==6) {
+			    //fillRect(x,80,10,130);
+			    
+			  //}
+			    //else if(i==7) {
+			    //fillRect(x,80,10,150);
+			    
+			  //}
+			    //else if(i==8) {
+			    //fillRect(x,80,10,170);
+			    
+			  //}
+			    //else if(i==9) {
+				 //fillRect(x,80,10,190);
+				 
+			 //}
+			  
+			     //else {
+			     //fillRect(x,80,10,200);
+			     
+			  //}
+			//x += 20;
+		//}
+	//}
+//}
+		
+	//4-3
+		//int x = 30;
+				//setColor(0,10,0);
+				//int i;
+				//for(i = 0; i < 10; i++) {
+					//if(i==0) {
+						//fillRect(x,80,10,100);
+						//setColor(0,0,0);
+				//	  }
+			//		    else if(i==1) {
+			//		    fillRect(x,80,10,100);
+				//	    setColor(15,15,15);
+					//  }
+					  //  else if(i==2) {
+//					    fillRect(x,80,10,100);
+	//				    setColor(30,30,30);
+		//			  }
+			//		    else if(i==3) {
+				//	    fillRect(x,80,10,100);
+					//    setColor(45,45,45);
+					  //}
+//					    else if(i==4) {
+	//				    fillRect(x,80,10,100);
+		//			    setColor(60,60,60);
+			//		  }
+				//	    else if(i==5) {
+					//    fillRect(x,80,10,100);
+					  //  setColor(75,75,75);
+					  //}
+					   // else if(i==6) {
+					    //fillRect(x,80,10,100);
+					    //setColor(90,90,90);
+			//		  }
+				//	    else if(i==7) {
+					//    fillRect(x,80,10,100);
+					  //  setColor(105,105,105);
+					  //}
+					    //else if(i==8) {
+					    //fillRect(x,80,10,100);
+					    //setColor(110,110,110);
+					  //}
+					    //else if(i==9) {
+						 //fillRect(x,80,10,100);
+						 //setColor(120,120,120);
+					//  }
+					
+					  //  else {
+						     //fillRect(x,80,10,200);
+						     //setColor(128,128,128);
+		
+					  //}
+					//x += 20;
+			 //	}
+	//}
+//}
+		
+		//EX
+		int x = 30;
+		setColor(0,10,0);
+		int i;
+		for(i = 0; i < 20; i++) {
+			if(i==0) {
+				fillRect(x,80,10,100);
+				setColor(0,0,0);
+			  }
+			    else if(i==1) {
+			    fillRect(x,80,10,100);
+			    setColor(15,15,15);
+			  }
+			    else if(i==2) {
+			    fillRect(x,80,10,100);
+			    setColor(30,30,30);
+			  }
+			    else if(i==3) {
+			    fillRect(x,80,10,100);
+			    setColor(45,45,45);
+			  }
+			    else if(i==4) {
+			    fillRect(x,80,10,100);
+			    setColor(60,60,60);
+			  }
+			    else if(i==5) {
+			    fillRect(x,80,10,100);
+			    setColor(75,75,75);
+			  }
+			    else if(i==6) {
+			    fillRect(x,80,10,100);
+			    setColor(90,90,90);
+			  }
+			    else if(i==7) {
+			    fillRect(x,80,10,100);
+			    setColor(105,105,105);
+			  }
+			    else if(i==8) {
+			    fillRect(x,80,10,100);
+			    setColor(110,110,110);
+			  }
+			    else if(i==9) {
+				 fillRect(x,80,10,100);
+				 setColor(120,120,120);
+			 }
+			    else if(i==11) {
+				fillRect(x,80,10,100);
+				setColor(105,105,105);
+			  }
+				 else if(i==12) {
+			    fillRect(x,80,10,100);
+			    setColor(90,90,90);
+			  }
+				 else if(i==13) {
+				 fillRect(x,80,10,100);
+				 setColor(75,75,75);
+			  }
+				 else if(i==14) {
+				 fillRect(x,80,10,100);
+				 setColor(45,45,45);
+			  }
+				 else if(i==15) {
+				 fillRect(x,80,10,100);
+				 setColor(30,30,30);
+			  }
+				 else if(i==16) {
+				 fillRect(x,80,10,100);
+				 setColor(15,15,15);
+			  }
+				 else if(i==17) {
+				 fillRect(x,80,10,100);
+				 setColor(10,10,10);
+			  }
+				 else if(i==18) {
+				 fillRect(x,80,10,100);
+				 setColor(7,7,7);
+		      }
+				 else if(i==19) {
+				 fillRect(x,80,10,100);
+				 setColor(5,5,5);
+			  }
+				 else if(i==20) {
+				  fillRect(x,80,10,100);
+				  setColor(0,0,0);
+			  }
+			     else {
+			     fillRect(x,80,10,100);
+			     setColor(128,128,128);
+			  }
+			x += 15;
+		}
+	}
+}
